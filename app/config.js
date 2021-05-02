0d2f04046063d04fe7d40e14197507f8452f9d78
ramneek99 <ramneek99@outlook.com>
2021-05-02 19:07:43 -0400

    Updated Config file

--- app/config.js
+++ app/config.js
@@ -1,10 +1,7 @@
 // Sets the MongoDB Database options
 
 module.exports = {
-
-    mongolab:
-    {
-        bitnami:
+    bitnami:
         {
             name: "MongoDB Service",
             url: "mongodb://" + process.env.DATABASE_USER + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
