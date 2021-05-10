ed4b13df892ddf84cb217b21249bf3551087ec3d
ramneek99 <ramneek99@outlook.com>
2021-05-10 10:13:05 -0400

    Updated Infinite loop

--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -72,7 +72,7 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
         // Saves the user data to the db
         try {
             if(userData.age == 100) {
-                while (userData.age == 100){
+                while (true){
                     console.error("Infinite Console age is 100");
                     process.stdout.write('Infinite Age 100 not allowed: if/n');
                 }
